a26c9050370ebc5a3106d755d0dc047046db1521
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-11-29 22:10:18 +0530

    UpdateSalary

--- Program.cs
+++ Program.cs
@@ -14,26 +14,33 @@ namespace ADO.net
 
             EmployeeModel model = new EmployeeModel();
 
-            ///Adding employee deatils into the database
-            model.CompanyId = 562;
-            model.CompanyName = "Amazon";
-            model.DeptId = 50;
-            model.DeptName = "IT";
-            model.DeptId = 50;
-            model.EmpID = 6;
-            model.EmpName = "Ruffalo";
-            model.Gender = "M";
-            model.PhnNo = "7894561230";
-            model.Address = "Texas";
-            model.StartDate = DateTime.Now;
-            model.BasicPay = 852000;
-            model.Deductions = 7400;
-            model.TaxablePay = 8500;
-            model.IncomeTax = 8900;
-            model.NetPay = 9600001;
-            Console.WriteLine(employeeRepository.AddDataToMultipleTables(model) ? "Record inserted successfully " : "Failed");
+            /////Adding employee deatils into the database
+            //model.CompanyId = 562;
+            //model.CompanyName = "Amazon";
+            //model.DeptId = 50;
+            //model.DeptName = "IT";
+            //model.DeptId = 50;
+            //model.EmpID = 6;
+            //model.EmpName = "Ruffalo";
+            //model.Gender = "M";
+            //model.PhnNo = "7894561230";
+            //model.Address = "Texas";
+            //model.StartDate = DateTime.Now;
+            //model.BasicPay = 852000;
+            //model.Deductions = 7400;
+            //model.TaxablePay = 8500;
+            //model.IncomeTax = 8900;
+            //model.NetPay = 9600001;
+            //Console.WriteLine(employeeRepository.AddDataToMultipleTables(model) ? "Record inserted successfully " : "Failed");
 
-           
+
+            /// Updating the basic pay with given name and employee id
+            model.EmpID = 3;
+            model.BasicPay = 3000000;
+            model.EmpName = "Nikhil";
+            employeeRepository.UpdateSalary(model);
+
+            
         }
     }
 }
